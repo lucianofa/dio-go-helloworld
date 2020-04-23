44f4546d743ed0d048decc9611a62dae71cd84fc
Luciano Faria <luciano@microstop.com.br>
2020-04-23 06:37:53 -0300

    configuração para Run no VsCode

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lucianofa/dio-go-helloworld
+
+go 1.14
--- hello.go
+++ hello.go
@@ -1,5 +1,6 @@
 package main
 import "fmt"
 func main() {
-        fmt.Printf("hello, word\n")
+    fmt.Println("Wlelcom to")
+    fmt.Print("this")
 }
